afcf017b2d1819790f766ffc72b4dff35127facb
dascse <145799966+dascse@users.noreply.github.com>
2023-09-27 22:12:45 +0530

    little bit code

---
+++ 1st task.c
@@ -0,0 +1,9 @@
+//
+// Created by Admin on 9/27/2023.
+//1st Assignment
+
+#include<stdio.h>
+int main()
+{
+    printf("hello world");
+}
---
+++ 1st task.h
@@ -0,0 +1,8 @@
+//
+// Created by Admin on 9/27/2023.
+//
+
+#ifndef C_ASSIGNMENT_1ST_TASK_H
+#define C_ASSIGNMENT_1ST_TASK_H
+
+#endif //C_ASSIGNMENT_1ST_TASK_H
