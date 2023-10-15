5c17935fe0591f84254c8b461c479801a95355d0
dascse <145799966+dascse@users.noreply.github.com>
2023-10-16 00:06:26 +0530

    Update 3rd task.c

--- 3rd task.c
+++ 3rd task.c
@@ -1,14 +1,16 @@
-//
-// Created by Admin on 10/3/2023.
-
+//Write a program in c to perform addition,substraction,multiplication and division of two variable by taking user input.
 #include<stdio.h>
-int main() {
-    for(int i=1;i<=5;i++)
-    {
-        for(int j=1;j<=i;j++)
-        {
-            printf("%d ",i);
-        }
-        printf("\n");
-    }
+int main()
+{
+    int a,b,c,d,e,f;
+    printf("Enter the number:");
+    scanf("%d\n%d",&a,&b);
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=b/a;
+    printf("Addition=%d\n",c);
+    printf("Substaction=%d\n",d);
+    printf("Multiplication=%d\n",e);
+    printf("Division=%d\n",f);
 }
