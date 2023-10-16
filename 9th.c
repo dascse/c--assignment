4a1c6903354946de8a226b6538a979a05c9601f3
dascse <145799966+dascse@users.noreply.github.com>
2023-10-16 21:52:28 +0530

    Create 9th.c

---
+++ 9th.c
@@ -0,0 +1,13 @@
+//
+// Created by Admin on 10/16/2023.
+//
+//Write a program to get the discriminant value of a quadratic equation.
+#include<stdio.h>
+int main()
+{
+    int a,b,c,D;
+    printf("Enter the values of A,B and C:\n");
+    scanf("%d%d%d",&a,&b,&c);
+    D=(b*b)-(4*a*c);
+    printf("The value of Discriminant is:%d\n",D);
+}
