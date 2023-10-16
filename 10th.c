6db9425b655705fe5b815bde6a60e89898a9bf00
dascse <145799966+dascse@users.noreply.github.com>
2023-10-16 21:52:01 +0530

    Create 10th.c

---
+++ 10th.c
@@ -0,0 +1,13 @@
+//
+// Created by Admin on 10/16/2023.
+//
+//Write a program to convert the value of temperature from degree celcius to degree fahrenheit.
+#include<stdio.h>
+int main()
+{
+    float c,f;
+    printf("Enter the temperature in celcius :\n");
+    scanf("%f",&c);
+    f=(c*9/5)+32;
+    printf("%f\n",f);
+}
